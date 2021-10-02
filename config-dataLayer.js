bd07252537be96299f9838c326b6dea031903854
Murilo Dias <murilodias6710@gmail.com>
2021-10-02 16:04:41 -0300

    Versão inicial do modelo personalizado

--- config-dataLayer.js
+++ config-dataLayer.js
@@ -15,27 +15,16 @@
       }
       const inExcludeList = window.ajrS && window.ajrS.length > 0 ? window.ajrS.filter(e => xhr.url.startsWith(e.url)).length > 0 : false
       if (!inExcludeList) {
-        const stores = [
-          { id: '5d56f2cbc1a5e60034475656', name: 'Accon Curitiba' },
-          { id: '5ec05c59d453ab004c08f1e7', name: 'Accon Delivery' },
-          { id: '5ecedd46fd8511004c4f5c24', name: 'Accon Nordeste' },
-          { id: '5d42e2a29ba6160034605f01', name: 'Accon Porto Alegre' },
-          { id: '5ec818bee2b733004c82e547', name: 'Accon Rio de Janeiro' },
-          { id: '5d0a82535a1cf10033cb0969', name: 'Accon São Paulo' }
-        ]
-
         if ((xhr.url.indexOf('details') !== -1) && (xhr.url.indexOf('store') !== -1)) {
           const eventLabel = (xhr.url.indexOf('widget=') !== -1) ? '' : xhr.responseText
           const obj = JSON.parse(eventLabel)
           const url = new URL(xhr.url)
-          stores.forEach(store => {
-            if (store.id === url.searchParams.get('store')) {
-              obj.store = {
-                id: store.id,
-                name: store.name
-              }
-            }
-          })
+
+          obj.store = {
+            id: url.searchParams.get('store'),
+            name: ''
+          }
+
           if (obj.price.originalPrice > 0) {
             obj.price.discount = obj.price.originalPrice - obj.price.actualPrice
           } else {
@@ -48,9 +37,22 @@
             view: JSON.stringify(obj)
           })
         }
+
+        if (xhr.url === 'https://api.accon.app/v1/order') {
+          const eventLabel = (xhr.url.indexOf('widget=') !== -1) ? '' : xhr.responseText
+          dataLayer.push({
+            event: 'accon',
+            order: eventLabel
+          })
+        }
       }
       clearInterval(intervalId)
     }, 1)
     return xhrSend.apply(this, [].slice.call(arguments))
   }
 })()
+
+const VIEW = []
+const LIST = []
+const CART = []
+const ORDER = []
